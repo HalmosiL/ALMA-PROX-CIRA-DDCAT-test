0cc334c6a1eecb691080ee375b72420c825d3614
HalmosiL <halmosilevent@gmail.com>
2023-02-02 15:06:30 +0100

    add

--- alma_prox_segmentation/dataset_ingredient.py
+++ alma_prox_segmentation/dataset_ingredient.py
@@ -10,16 +10,6 @@ from utils import label_map_cityscapes
 
 dataset_ingredient = Ingredient('dataset')
 
-
-@dataset_ingredient.named_config
-def pascal_voc_2012():
-    name = 'pascal_voc_2012'
-    root = 'data'
-    split = 'val'
-    size = 512
-    num_images = None
-
-
 @dataset_ingredient.named_config
 def cityscapes():
     name = 'cityscapes'
@@ -30,28 +20,10 @@ def cityscapes():
 
 
 @dataset_ingredient.capture
-def get_pascal_voc_2012(root: str, size: int, split: str,
-                        num_images: Optional[int] = None) -> Tuple[DataLoader, Optional[Callable]]:
-    transform = transforms.Compose([
-        transforms.Resize(size),
-        transforms.ToTensor(),
-    ])
-    target_transform = transforms.Compose([
-        transforms.PILToTensor(),
-        transforms.Resize(size, interpolation=InterpolationMode.NEAREST)
-    ])
-    dataset = VOCSegmentation(root=root, year='2012', image_set=split,
-                              transform=transform, target_transform=target_transform)
-    if num_images is not None:
-        assert num_images <= len(dataset)
-        dataset = Subset(dataset, indices=list(range(num_images)))
-    loader = DataLoader(dataset=dataset, shuffle=False)
-    return loader, None
-
-
-@dataset_ingredient.capture
 def get_cityscapes(root: str, size: int, split: str,
                    num_images: Optional[int] = None, batch_size: int = 1) -> Tuple[DataLoader, Optional[Callable]]:
+    print("Size:", size)
+
     transform = transforms.Compose([
         transforms.Resize(size),
         transforms.ToTensor(),
