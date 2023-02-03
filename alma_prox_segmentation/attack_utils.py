8170c979d899907c692b728a63b99c58b37149e5
HalmosiL <halmosilevent@gmail.com>
2023-02-03 17:37:16 +0100

    auto-commit

--- alma_prox_segmentation/attack_utils.py
+++ alma_prox_segmentation/attack_utils.py
@@ -58,9 +58,7 @@ def run_attack(
             else:
                 attack_label = label
 
-            logits = model(image)
-
-            print(len(logits))
+            _, logits = model(image)
 
             if i == 0:
                 num_classes = logits.size(1)
